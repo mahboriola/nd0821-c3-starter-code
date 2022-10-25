0c489f3b24f929f4bfc157a46e725e3678c8c4f7
Marcos Aurélio Hermogenes Boriola <mahboriola@gmail.com>
2022-10-25 17:55:28 -0300

    add test set scores

--- starter/starter/train_model.py
+++ starter/starter/train_model.py
@@ -5,12 +5,13 @@ from sklearn.model_selection import train_test_split
 # Add the necessary imports for the starter code.
 import pandas as pd
 import pickle
+import json
 
 import sys
 sys.path.append('./starter/starter/ml')
 
 from data import process_data
-from model import train_model
+from model import train_model, inference, compute_model_metrics
 
 # Add code to load in the data.
 data = pd.read_csv('starter/data/census.csv')
@@ -40,6 +41,17 @@ X_test, y_test, _, _ = process_data(
 
 # Train and save a model.
 model = train_model(X_train, y_train)
+y_pred = inference(model, X_test)
+precision, recall, fbeta = compute_model_metrics(y_test, y_pred)
+
+scores = {
+    'precision': round(precision, ndigits=3),
+    'recall': round(recall, ndigits=3),
+    'fbeta': round(fbeta, ndigits=3)
+}
+
+with open('starter/screenshots/test_set_scores.json', 'w') as f:
+    json.dump(scores, f, indent=4)
 
 with open('starter/model/model.pkl', 'wb') as model_file, \
      open('starter/model/encoder.pkl', 'wb') as encoder_file, \
